7ec17bd6de4393dd10a7d32108533b000f2a3512
skumar1998 <93005881+skumar1998@users.noreply.github.com>
2022-01-17 03:52:22 -0500

    Add files via upload

---
+++ cplusplus.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Area(l,b) (l*b)
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of A:";
+    cin>>a;
+    cout<<"Enter the value of B:";
+    cin>>b;
+    cout<<"The value of Area is :";
+    cout<<Area(a,b);
+    return 0;
+
+}
